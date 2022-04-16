becc8a8581b1c517be10e65a76526f7c89a12eb5
Sarang Kapadia <sarangksk@gmail.com>
2022-04-15 17:20:06 -0700

    colors , icons

--- src/header.tsx
+++ src/header.tsx
@@ -2,29 +2,51 @@ import React from "react";
 import { Title } from "./title";
 import "./style/header.css";
 import { IconButton } from "@mui/material";
-// import MenuIcon from "@mui/icons-material/Menu";
-import { Menu, HelpOutline, Leaderboard, Settings } from "@mui/icons-material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import {
+  SettingsRounded,
+  MenuRounded,
+  HelpRounded,
+  LeaderboardRounded,
+} from "@mui/icons-material";
+
+// move this to a useEffect
+const root = document.querySelector(":root")!;
+const rootStyle = getComputedStyle(root);
+
+const fontColor = rootStyle.getPropertyValue("--fontColor");
+
+const outerTheme = createTheme({
+  palette: {
+    primary: {
+      main: fontColor.trim(), // hex versions of --var(fontColor)
+    },
+  },
+});
 
 export const Header: React.FunctionComponent = () => {
   return (
-    <div className={"header"}>
-      <div className={"menuleft"}>
-        <IconButton aria-label="Menu" color="primary" size="large">
-          <Menu />
-        </IconButton>
-        <IconButton aria-label="Instruction" color="primary" size="large">
-          <HelpOutline />
-        </IconButton>
+    <ThemeProvider theme={outerTheme}>
+      <div className={"header"}>
+        <div className={"menuleft"}>
+          <IconButton aria-label="Menu" color="primary" size="small">
+            <MenuRounded />
+          </IconButton>
+          <IconButton aria-label="Instruction" color="primary" size="small">
+            <HelpRounded />
+          </IconButton>
+        </div>
+        <Title />
+        <div className={"menuright"}>
+          <IconButton aria-label="Stats" color="primary" size="small">
+            <LeaderboardRounded />
+          </IconButton>
+          <IconButton aria-label="Settings" color="primary" size="small">
+            <SettingsRounded />
+          </IconButton>
+        </div>
       </div>
-      <Title />
-      <div className={"menuright"}>
-        <IconButton aria-label="Stats" color="primary" size="large">
-          <Leaderboard />
-        </IconButton>
-        <IconButton aria-label="Settings" color="primary" size="large">
-          <Settings />
-        </IconButton>
-      </div>
-    </div>
+    </ThemeProvider>
   );
 };
