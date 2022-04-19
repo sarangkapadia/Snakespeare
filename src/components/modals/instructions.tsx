1fcbbe8cc8acc23a081ae26921a850a618918e12
Sarang Kapadia <sarangksk@gmail.com>
2022-04-18 23:17:19 -0700

    instructions modal decent state

--- src/components/modals/instructions.tsx
+++ src/components/modals/instructions.tsx
@@ -12,23 +12,29 @@ export const InstructionsModal: React.FunctionComponent<IInstructionsModal> =
     const { action, onCloseInstructions } = props;
     return (
       <div
-        className={
-          action
-            ? "instructionsModalContainerIn"
-            : "instructionsModalContainerOut"
-        }
+        className={action ? "instructionsOverlayIn" : "instructionsOverlayOut"}
       >
-        <h2 className="textInstructions">{"How to play"}</h2>
-        <h4 className="textInstructions">
-          {"Swipe up, down, left, right to move the snake"}
-        </h4>
-        <h4 className="textInstructions">
-          {"Consume the letters to form a five letter word"}
-        </h4>
-        <img src={snakeSmile} alt={"funny snake GIF"} />
-        <button className={"playButton"} onClick={onCloseInstructions}>
-          Play
-        </button>
+        <div className={"modalContainer"}>
+          <div className={"header"}>
+            <h3>How to play</h3>
+            <button className={"close"} onClick={onCloseInstructions}>
+              X
+            </button>
+          </div>
+          <p>Swipe anywhere on the screen to start the game.</p>
+          <p>
+            Navigate your snake by swiping left, right, up or down. This will
+            change the direction of the snake's head.
+          </p>
+          <p>
+            Steer the snake to capture the letters in the correct order and
+            create a 5 letter word using the randomly placed letters. Score 100
+            points per word + bonus for speed.
+          </p>
+          <p>Avoid colliding the snake's head with it's own body.</p>
+
+          <img src={snakeSmile} alt={"funny snake GIF"} />
+        </div>
       </div>
     );
   });
