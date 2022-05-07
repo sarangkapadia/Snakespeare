8ec26d9eb9cb9d9911946780f902b429e0e0da95
Sarang Kapadia <sarangksk@gmail.com>
2022-05-06 23:28:41 -0700

    more button styling

--- src/button.tsx
+++ src/button.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style/button.css";
+import { ReactComponent as Share } from "./share.svg";
 
 interface IButtonProps {
   onClick: () => void;
@@ -10,7 +11,10 @@ export const Button: React.FunctionComponent<IButtonProps> = React.memo(
   (props) => {
     return (
       <button className={"button"} onClick={props.onClick}>
-        {props.label}
+        <div className="buttonContainer">
+          {props.label}
+          <Share className={"shareLogo"} />
+        </div>
       </button>
     );
   }
